4909dc6c9d210228952dc63499e7dbbf43974fe1
AbGhost-cyber <xplendo@gmail.com>
2023-04-24 18:21:05 +0800

    week 4

---
+++ week3/logistic_regression.py
@@ -0,0 +1,95 @@
+import torch.nn as nn
+import torch
+import matplotlib.pyplot as plt
+
+# Set the random seed
+
+torch.manual_seed(2)
+
+z = torch.arange(-100, 100, 0.1).view(-1, 1)
+print("The tensor: ", z)
+
+sig = nn.Sigmoid()
+yhat = sig(z)
+
+plt.plot(z.numpy(), yhat.numpy())
+plt.xlabel('z')
+plt.ylabel('yhat')
+
+yhat = torch.sigmoid(z)
+plt.plot(z.numpy(), yhat.numpy())
+
+# Create x and X tensor
+x = torch.tensor([[1.0]])
+X = torch.tensor([[1.0], [100]])
+print('x = ', x)
+print('X = ', X)
+
+# Use sequential function to create model
+model = nn.Sequential(nn.Linear(1, 1), nn.Sigmoid())
+
+# Print the parameters
+print("list(model.parameters()):\n ", list(model.parameters()))
+print("\nmodel.state_dict():\n ", model.state_dict())
+
+# The prediction for X
+yhat = model(X)
+
+# Create and print samples
+x = torch.tensor([[1.0, 1.0]])
+X = torch.tensor([[1.0, 1.0], [1.0, 2.0], [1.0, 3.0]])
+print('x = ', x)
+print('X = ', X)
+
+# Create new model using nn.sequential()
+model = nn.Sequential(nn.Linear(2, 1), nn.Sigmoid())
+
+
+# Create logistic_regression custom class
+
+class logistic_regression(nn.Module):
+
+    # Constructor
+    def __init__(self, n_inputs):
+        super(logistic_regression, self).__init__()
+        self.linear = nn.Linear(n_inputs, 1)
+
+    # Prediction
+    def forward(self, x):
+        yhat = torch.sigmoid(self.linear(x))
+        return yhat
+
+
+# Create x and X tensor
+x = torch.tensor([[1.0]])
+X = torch.tensor([[-100], [0], [100.0]])
+print('x = ', x)
+print('X = ', X)
+
+# Create logistic regression model
+model = logistic_regression(1)
+
+# Print parameters
+print("list(model.parameters()):\n ", list(model.parameters()))
+print("\nmodel.state_dict():\n ", model.state_dict())
+
+# Make the prediction of X
+yhat = model(X)
+print("The prediction result: \n", yhat)
+
+# Create logistic regression model
+model = logistic_regression(2)
+
+# Create x and X tensor
+x = torch.tensor([[1.0, 2.0]])
+X = torch.tensor([[100, -100], [0.0, 0.0], [-100, 100]])
+print('x = ', x)
+print('X = ', X)
+
+# Make the prediction of x
+yhat = model(x)
+print("The prediction result: \n", yhat)
+
+# Make the prediction of X
+yhat = model(X)
+print("The prediction result: \n", yhat)
